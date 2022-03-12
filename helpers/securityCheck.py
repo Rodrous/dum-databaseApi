6013d8d7ca5d83f377cfebc99250f08c0afe827b
Rodrous <59244346+Rodrous@users.noreply.github.com>
2022-03-12 19:08:10 +0530

    Security Main

---
+++ helpers/securityCheck.py
@@ -0,0 +1,25 @@
+from typing import Dict
+
+from passlib.context import CryptContext
+from dataclasses import dataclass
+from logic_layer.backend import authenticateUser
+
+
+@dataclass
+class UserInfoVerification:
+    username: str
+    password: str
+    password_context: CryptContext = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+    def create_password_hash(self):
+        return self.password_context.hash(self.password)
+
+    async def authenticate(self):
+        user_verification: Dict | None = await authenticateUser(self.username)
+        if user_verification:
+            if self.verify_password(self.password, user_verification):
+                return True
+        return False
+
+    def verify_password(self, plain_password, hashed_password):
+        return self.password_context.verify(plain_password, hashed_password)
